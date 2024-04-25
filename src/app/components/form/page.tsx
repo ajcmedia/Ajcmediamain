4b051abad854591bab8d0bd970551cc3868c9a69
MiykelC <105033752+MiykelC@users.noreply.github.com>
2024-04-25 01:12:10 -0700

    Worked on images link through google photos

---
+++ src/app/components/form/page.tsx
@@ -0,0 +1,12 @@
+
+import styles from "./form.module.scss";
+import Image from 'next/image'
+
+
+export default function Form() {
+  return (
+    <main className={styles.mainFormContainer}>
+      
+    </main>
+  )
+}
