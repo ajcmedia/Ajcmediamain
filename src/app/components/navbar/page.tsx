b4a426c5f3cf1128b9884a51052bc62a012838f5
MiykelC <michaelchua1021@gmail.com>
2024-06-28 02:11:44 -0700

    worked on responsiveness of navbar

--- src/app/components/navbar/page.tsx
+++ src/app/components/navbar/page.tsx
@@ -1,88 +1,68 @@
-
+"use client";
+import React, { useState } from "react";
 import styles from "./navbar.module.scss";
-import Image from "next/image"
+import Image from "next/image";
 
 export default function Navbar() {
-  return (
-    // <main className={styles.navcontainer}>
-    //   <div className={styles.navleft}>
-    //     <a href="#">Home</a>
-    //     <a href="#">About</a>
-    //   </div>
-    //   <Image src="/logo1.svg" alt="logo" width={80} height={80} />
-    //   <div className={styles.navright}>
-    //     <a href="#">Portfolio</a>
-    //     <button>Contact me</button>
-    //   </div>
-    // </main>
-    <main className={styles.navcontainer}>
-      <div className={styles.navleft}>
-        <Image src="/logo1.svg" alt="logo" width={70} height={70} />
-        <a href="#">Home</a>
-        <a href="#">About</a>
-        <a href="#">Portfolio</a>
-      </div>
-      <div className={styles.navright}>
-        <a href="#">Contact me</a>
-        <div className={styles.socials}>
-          <div className={styles.socialLinks}>
-            <Image
-              width={25}
-              height={25}
-              src="/facebook.svg"
-              alt="logo"
-              className={styles.fb1}
-            />
-            <a href="https://www.facebook.com/jayson.chua.750">
-              <Image
-                width={25}
-                height={25}
-                src="/facebook_hover.svg"
-                alt="logo"
-                className={styles.socialshover}
-              />
-            </a>
-          </div>
+  const [isOpen, setIsOpen] = useState(false);
 
-          <div className={styles.socialLinks}>
-            <Image
-              width={25}
-              height={25}
-              src="/messenger.svg"
-              alt="logo"
-              className={styles.ms1}
-            />
-            <a href="https://www.m.me/jayson.chua.750">
-              <Image
-                width={25}
-                height={25}
-                src="/messenger_hover.svg"
-                alt="logo"
-                className={styles.socialshover}
-              />
-            </a>
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <main className={styles.navcontainer}>
+      <div className={styles.nav_sub_container}>
+        <div className={styles.navleft2}>
+          <Image src="/logo1.svg" alt="logo" width={80} height={80} className={styles.logosmall} />
+        </div>
+
+        <div className={styles.navleft}>
+          <Image src="/logo1.svg" alt="logo" width={70} height={70} />
+          <a href="#">Home</a>
+          <a href="#">About</a>
+          <a href="#">Portfolio</a>
+        </div>
+
+        <div className={`${styles.navright} ${isOpen ? styles.show : ""}`}>
+          <div className={`${styles.navlinks}`}>
+            <a href="#">Home</a>
+            <a href="#">About</a>
+            <a href="#">Portfolio</a>
           </div>
-          <div className={styles.socialLinks}>
-            <Image
-              width={25}
-              height={25}
-              src="/instagram.svg"
-              alt="logo"
-              className={styles.ig1}
-            />
-            <a href="https://www.instagram.com/jaysonchua_/">
-              <Image
-                width={25}
-                height={25}
-                src="/instagram_hover.svg"
-                alt="logo"
-                className={styles.socialshover}
-              />
-            </a>
+          <a href="#">Contact me</a>
+          <div className={styles.socials}>
+            <div className={styles.socialLinks}>
+              <Image width={25} height={25} src="/facebook.svg" alt="logo" className={styles.fb1} />
+              <a href="https://www.facebook.com/jayson.chua.750">
+                <Image width={25} height={25} src="/facebook_hover.svg" alt="logo" className={styles.socialshover} />
+              </a>
+            </div>
+
+            <div className={styles.socialLinks}>
+              <Image width={25} height={25} src="/messenger.svg" alt="logo" className={styles.ms1} />
+              <a href="https://www.m.me/jayson.chua.750">
+                <Image width={25} height={25} src="/messenger_hover.svg" alt="logo" className={styles.socialshover} />
+              </a>
+            </div>
+
+            <div className={styles.socialLinks}>
+              <Image width={25} height={25} src="/instagram.svg" alt="logo" className={styles.ig1} />
+              <a href="https://www.instagram.com/jaysonchua_/">
+                <Image width={25} height={25} src="/instagram_hover.svg" alt="logo" className={styles.socialshover} />
+              </a>
+            </div>
+          </div>
+        </div>
+
+        <div className={`${styles.hamburger} ${isOpen ? styles.open : ""}`} onClick={toggleMenu}>
+          <div className={styles.lines}>
+            <div></div>
+            <div></div>
+            <div></div>
           </div>
         </div>
       </div>
-
     </main>
-  )
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
   return (
     <main className={styles.landingContainer}>
 
-      <div className={styles.landingContainer2}>
+      {/* <div className={styles.landingContainer2}>
          <Navbar />
         <div className={styles.landingBanner}>
          
@@ -18,63 +18,6 @@ export default function Home() {
 
             <h1>My name is Jayson, I am a <span>Photographer</span></h1>
             <p>I wanted to enhance my photography skills by sharing what i am capable of to others</p>
-            {/* <div className={styles.socials}>
-              <div className={styles.socialLinks}>
-                <Image
-                  width={30}
-                  height={30}
-                  src="/facebook.svg"
-                  alt="logo"
-                  className={styles.fb1}
-                />
-                <a href="https://www.facebook.com/jayson.chua.750">
-                  <Image
-                    width={30}
-                    height={30}
-                    src="/facebook_hover.svg"
-                    alt="logo"
-                    className={styles.socialshover}
-                  />
-                </a>
-              </div>
-
-              <div className={styles.socialLinks}>
-                <Image
-                  width={30}
-                  height={30}
-                  src="/messenger.svg"
-                  alt="logo"
-                  className={styles.ms1}
-                />
-                <a href="https://www.m.me/jayson.chua.750">
-                  <Image
-                    width={30}
-                    height={30}
-                    src="/messenger_hover.svg"
-                    alt="logo"
-                    className={styles.socialshover}
-                  />
-                </a>
-              </div>
-              <div className={styles.socialLinks}>
-                <Image
-                  width={30}
-                  height={30}
-                  src="/instagram.svg"
-                  alt="logo"
-                  className={styles.ig1}
-                />
-                <a href="https://www.instagram.com/jaysonchua_/">
-                  <Image
-                    width={30}
-                    height={30}
-                    src="/instagram_hover.svg"
-                    alt="logo"
-                    className={styles.socialshover}
-                  />
-                </a>
-              </div>
-            </div> */}
           </div>
         </div>
         <div className={styles.collageMain}>
@@ -99,10 +42,10 @@ export default function Home() {
           </div>
         </div>
         <Footer />
-      </div>
+      </div> */}
 
-
-
+     
+        <Navbar />
 
     </main>
   );
