fb80c9dc9ed4ae2a10de1871150c686e6bf36cb1
MiykelC <105033752+MiykelC@users.noreply.github.com>
2025-09-16 22:26:34 -0700

    test push

--- src/app/components/aboutme/page.tsx
+++ src/app/components/aboutme/page.tsx
@@ -12,7 +12,7 @@ export default function Aboutme() {
                     <div className={styles.imageContainer}><Image width={500} height={500} src="/image1.jpg" alt="myImage" /></div>
                     <p>As a freelance photographer based in Vancouver, originally from the Philippines, I focus on capturing meaningful moments with creativity and empathy. While specializing in portraits, I also cover events like baby showers, gender reveals, birthdays, and weddings, which inspire me to capture cherished memories. My work is driven by a passion for storytelling through photography</p>
                     <div className={styles.info}>
-                        <a><span>&#124;</span> Doing photography since 2022 </a>
+                        <a><span>&#124;</span> Doing photography since 2021 </a>
                         <a><span>&#124;</span> Efficient in editing using Adobe photoshop </a>
                         <a><span>&#124;</span> Videographer partner for events </a>
                        
